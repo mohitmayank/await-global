2a2449aa6b87668ad8ad58e583e94f0b50c6db45
Mohit Jain <mayank.mohit@gmail.com>
2019-02-15 19:46:56 +0530

    inital version

---
+++ index.js
@@ -0,0 +1,22 @@
+function getGlobal() { 
+  if (typeof globalThis !== 'undefined') { return globalThis; } 
+  if (typeof self !== 'undefined') { return self; } 
+  if (typeof window !== 'undefined') { return window; } 
+  if (typeof global !== 'undefined') { return global; } 
+  throw new Error('unable to locate global object'); 
+} 
+
+module.exports = function awaitGlobal(key, wait) {
+  const root = getGlobal();
+  return new Promise((resolve, reject) => {
+    let timer;
+    const loop = () => {
+      if(root[key]) {
+        resolve(root[key]);
+        clearInterval(timer);
+      }
+    };
+    timer = setInterval(loop, wait || 300);
+    waitLoop();
+  });
+};

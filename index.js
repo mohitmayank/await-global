e7603db0d21ac616c0542856001e8d15b8a91149
Mohit <mayank.mohit@gmail.com>
2020-09-09 00:34:41 +0530

    adds max attempts

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ function getGlobal() {
   throw new Error('unable to locate global object'); 
 } 
 
-module.exports = function awaitGlobal(key, wait) {
+module.exports = function awaitGlobal(key, wait, maxAttempts) {
   const root = getGlobal();
+  let attempts = maxAttempts || 3;
   return new Promise((resolve, reject) => {
     let timer;
     const loop = () => {
+      attempts--;
       if(root[key]) {
         resolve(root[key]);
+        clearInterval(timer);
+      } else if (!attempts) {
+        reject(new Error('Too many attempts'));
         clearInterval(timer);
       }
     };
